a796d3fdc16db0da386283e646fb4f8249a42085
Kha Tran <khadieptran@gmail.com>
2021-07-07 11:40:07 -0400

    Create function playRound handling comparison of user and computer picks

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ function computerPlay() {
     return computerPick;
 }
 
+function playRound(playerSelection, computerSelection) {
+    let message = `You pick ${playerSelection}, and the computer picks ${computerSelection}`;
+    let winningMsg = `${message}\nYou win! ${playerSelection} beats ${computerSelection}`;
+    let losingMsg = `${message}\nYou lose! ${computerSelection} beats ${playerSelection}`;
+    if (playerSelection.toLowerCase() === computerSelection) {
+        return(`${message}\nIt's a draw.`);
+    } else {
+        if (playerSelection.toLowerCase() === 'rock') {
+            if (computerSelection === 'paper') {
+                return(losingMsg);
+            } else {
+                return(winningMsg);
+            }
+        } else if (playerSelection.toLowerCase() === 'paper') {
+            if (computerSelection === 'rock') {
+                return(winningMsg);
+            } else {
+                return(losingMsg);
+            }
+        } else if (playerSelection.toLowerCase() === 'scissor') {
+            if (computerSelection === 'rock') {
+                return(losingMsg);
+            } else {
+                return(winningMsg);
+            }
+        }
+    }
+}
