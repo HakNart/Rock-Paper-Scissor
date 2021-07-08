6336aa9f8648fa95a230bfb1ccc15dc598835b07
Kha Tran <khadieptran@gmail.com>
2021-07-08 08:43:33 -0400

    Disable log for computer choice

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function computerPlay() {
     let choices = ['rock', 'paper', 'scissor'];
 
     let computerPick = choices[Math.floor(Math.random()*choices.length)];
-    console.log(computerPick);
+    // console.log(computerPick);
     return computerPick;
 }
 
